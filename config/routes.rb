b665e19253390234dde418d1737e54e5c7610ba6
MaoOkadome <okdokd0224@gmail.com>
2018-02-16 10:56:37 +0000

    week3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'blogs/index' => 'blogs#index'
+  get '/blogs' => 'blogs#index'
+  get '/blogs/new' => 'blogs#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #get '/top' => 'root#top'
+
+  post '/blogs' => 'blogs#create' 
+  get  '/blogs/:id' => 'blogs#show'
+  #get '/blogs'
 end
